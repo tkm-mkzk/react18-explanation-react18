92fa9fae9615d21845ad72e3b79f1ff22bc9bf05
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-07-03 08:41:19 +0900

    section3

---
+++ src/components/AutoBatchEventHandler.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { flushSync } from 'react-dom'
+
+export const AutoBatchEventHandler = () => {
+  console.log('AutoBatchEventHandler')
+
+  const [state1, setState1] = useState<number>(0)
+  const [state2, setState2] = useState<number>(0)
+
+  const onClickUpdateButton = () => {
+    console.log(state1)
+    flushSync(() => {
+      setState1((state1) => state1 + 1)
+    })
+    console.log(state1)
+    setState2((state2) => state2 + 1)
+  }
+
+  return (
+    <div>
+      <p>Automatic Batching確認用（イベントハンドラ）</p>
+      <button onClick={onClickUpdateButton}>State更新！</button>
+      <p>State: {state1}</p>
+      <p>State: {state2}</p>
+    </div>
+  )
+}
